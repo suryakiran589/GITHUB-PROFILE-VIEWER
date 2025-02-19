24a9a7786329b1781f60276625baae678f15f4b0
surya <suryakirank153@gmail.com>
2025-02-19 22:36:43 +0530

    second commit

--- src/GetInfo.js
+++ src/GetInfo.js
@@ -2,6 +2,10 @@ import React, { useEffect, useState } from "react";
 
 function GetInfo({ prop }) {
   const [fdata, setFdata] = useState(null);
+  const sty={
+   
+  }
+
   useEffect(() => {
     try {
       async function getData(prop) {
@@ -18,15 +22,19 @@ function GetInfo({ prop }) {
   }, [prop]);
   
   return (fdata!=null ?
-  <div id="content">
+  <div id="content" style={sty
+  
+  }>
     <div>
 
     <img src={fdata?.avatar_url}/>
     </div>
     <div id="info">
 
-    <h1>Name -{fdata?.name}</h1>
-    <h1>followers {fdata?.followers}</h1>
+    {fdata && fdata.name ?<h1 className="name">Name -{fdata?.name}</h1> : <h1 className="name">Name -{fdata?.login}</h1>}
+    <span>followers {fdata?.followers}</span>
+    <span>following {fdata?.following}</span>
+    {fdata && fdata.bio && <h1>Bio- {fdata?.bio}</h1>}
     
     <a href={"https://github.com/"+prop}>Open Profile</a>  </div></div>:null)
 }
