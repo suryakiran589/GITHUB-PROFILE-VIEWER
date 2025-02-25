82421e70effd3cb0533069bab6127cf8945f896f
surya <suryakirank153@gmail.com>
2025-02-25 15:45:14 +0530

    final

--- App.js
+++ App.js
@@ -1,30 +1,39 @@
-import React,{useState,useEffect} from "react"
-import ReactDom from "react-dom/client"
-import GetInfo from "./src/GetInfo"
-import { Typewriter } from "react-simple-typewriter"
+import React, { useState, useEffect } from "react";
+import ReactDom from "react-dom/client";
+import GetInfo from "./src/GetInfo";
+import { Typewriter } from "react-simple-typewriter";
 
-function App()
-{
-    const [pid,setPid] = useState("")
-    const [curid,setCurid] = useState("")
-    const [dark,setDark] =useState(false)
-    
-        const body= document.body
-        body.style.background=dark ? "black" : "rgb(235, 235, 235)"
+function App() {
+  const [pid, setPid] = useState("");
+  const [curid, setCurid] = useState("");
+  const [dark, setDark] = useState(false);
+  const [isHovered, setIsHovered] = useState(false);
+  const sty = {
+    background: "transparent",
+    border: dark ? "1px solid white" : "1px solid black",
+    color: isHovered ? (dark ? "black" : "white") : dark ? "white" : "black",
+    borderRadius: "7px",
+    backgroundColor: isHovered ? (dark ? "white" : "black") : "transparent",
+  };
 
-   
-    useEffect(()=>{
-    },[])
-    return (
-        <div className="container">
-            <div id="inner">
+  const body = document.body;
+  body.style.background = dark ? "black" : "rgb(235, 235, 235)";
 
-            <div id="type">
-            <div>
-
-            <h1 id="heading" style={{color:dark?"rgb(16, 239, 243)":"#FFD700"}}> GitHub Profile Finder</h1>
-            </div>
-            {/* <Typewriter
+  useEffect(() => {}, []);
+  return (
+    <div className="container">
+      <div id="inner">
+        <div id="type">
+          <div>
+            <h1
+              id="heading"
+              style={{ color: dark ? "rgb(16, 239, 243)" : "#2d3436" }}
+            >
+              {" "}
+              GitHub Profile Finder
+            </h1>
+          </div>
+          {/* <Typewriter
             words={['GitHub','Profile','Finder']}
             loop={5}
             cursor
@@ -35,28 +44,50 @@ function App()
             // onLoopDone={handleDone}
             // onType={handleType}
             /> */}
-            <div>
-
-            <button id="dark-mode"  style={{backgroundColor:"rgb(16, 239, 243)",border:dark?"0px solid white":"0px solid black"}} onClick={()=>setDark(!dark)}>{dark ? "⏾" : "☼"}</button>
-            </div>
-            </div>
-            <div id="inp">
-            <input id="textbox" style={{backgroundColor:dark?"rgb(198, 196, 196)":"rgba(221, 221, 221, 0.86)",color:dark?"black":"white"}} placeholder="Username..." type="text" value={pid} onChange={(e)=>setPid(e.target.value)}></input>
-            <button style={{backgroundColor:"rgb(16, 239, 243)",color:"black"}} onClick={()=>{
-                setCurid(pid)
-            }}>Search</button>
-            </div>
-
-
-            {
-                curid && curid.length >0 ? < GetInfo prop={curid}/>
-                :null
-            }
-            
-            </div>
+          <div>
+            <button
+              id="dark-mode"
+              style={sty}
+              onMouseEnter={() => setIsHovered(true)}
+              onMouseLeave={() => setIsHovered(false)}
+              onClick={() => setDark(!dark)}
+            >
+              {dark ? "☼" : "⏾"}
+            </button>
+          </div>
         </div>
-    )
+        <div id="inp">
+          <input
+            id="textbox"
+            style={{
+              backgroundColor: dark
+                ? "rgb(198, 196, 196)"
+                : "rgba(221, 221, 221, 0.86)",
+              color: "black",
+            }}
+            placeholder="Username..."
+            type="text"
+            value={pid}
+            onChange={(e) => setPid(e.target.value)}
+          ></input>
+          <button
+            style={{
+              backgroundColor: dark ? "rgb(16, 239, 243)" : "#16a085",
+              color: dark ? "black" : "white",
+            }}
+            onClick={() => {
+              setCurid(pid);
+            }}
+          >
+            Search
+          </button>
+        </div>
+
+        {curid && curid.length > 0 ? <GetInfo prop={curid} dark={dark} /> : null}
+      </div>
+    </div>
+  );
 }
 
-const root = ReactDom.createRoot(document.getElementById("root"))
-root.render(<App/>)
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
--- src/GetInfo.js
+++ src/GetInfo.js
@@ -1,11 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-function GetInfo({ prop }) {
+function GetInfo({ prop ,dark}) {
   const [fdata, setFdata] = useState(null);
-  const [isError,setError]=useState(false)
-  const sty={
-   
-  }
+  const [isError, setError] = useState(false);
+  const sty = {color:dark?"#E0E0E0":"black"};
 
   useEffect(() => {
     try {
@@ -13,43 +11,47 @@ function GetInfo({ prop }) {
         console.log(prop);
         const data = await fetch(`https://api.github.com/users/${prop}`);
         const results = await data.json();
-        console.log('res',results);
-        if(results.message == "Not Found")
-        {
-          setError(true)
-        }
-        else
-        {
+        console.log("res", results);
+        if (results.message == "Not Found") {
+          setError(true);
+        } else {
           setFdata(results);
-          setError(false)
+          setError(false);
         }
       }
       getData(prop);
     } catch (error) {
       console.log(error, "error");
-
     }
   }, [prop]);
-  
-  return (!isError ? fdata!=null && fdata.id ?
-  <div id="content" style={sty
-  
-  }>
-    <div>
 
-    <img src={fdata?.avatar_url}/>
-    </div>
-    <div id="info">
-
-    {fdata && fdata.name ?<h1 className="name">Name -{fdata?.name}</h1> : <h1 className="name">Name -{fdata?.login}</h1>}
-    <h1>
-    following {fdata?.following}
-      {/* <span>followers {fdata?.followers}</span> */}
-      </h1>
-    <h1>followers {fdata?.followers}</h1>
-    {fdata && fdata.bio && <h1>Bio- {fdata?.bio}</h1>}
-    
-    <a href={"https://github.com/"+prop}>Open Profile</a>  </div></div>:<h1>loading</h1> : <h1>error</h1>)
+  return !isError ? (
+    fdata != null && fdata.id ? (
+      <div id="content" style={sty}>
+        <div>
+          <img src={fdata?.avatar_url} />
+        </div>
+        <div id="info">
+          {fdata && fdata.name ? (
+            <h1 className="name" style={sty}>Name -{fdata?.name}</h1>
+          ) : (
+            <h1 className="name" style={sty}>Name -{fdata?.login}</h1>
+          )}
+          <h1 style={sty}>
+            following {fdata?.following}
+            {/* <span>followers {fdata?.followers}</span> */}
+          </h1>
+          <h1 style={sty}>followers {fdata?.followers}</h1>
+          {fdata && fdata.bio && <h1 style={sty}>Bio- {fdata?.bio}</h1>}
+          <a href={"https://github.com/" + prop}>Open Profile</a>{" "}
+        </div>
+      </div>
+    ) : (
+      <h1>loading</h1>
+    )
+  ) : (
+    <h1 style={sty}>Coudn't Find Username</h1>
+  );
 }
 
 export default GetInfo;
